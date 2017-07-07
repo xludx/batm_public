1ea50dadc8f2664e860947b288636b240b204aeb
Juha Kovanen <juha.kovanen@gmail.com>
2017-07-07 15:27:44 +0300

    BPUB-241 fix for Particl

--- server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/particl/wallets/ParticldRPCWallet.java
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/particl/wallets/ParticldRPCWallet.java
@@ -107,7 +107,7 @@ public class ParticldRPCWallet implements IWallet{
         }
         try {
             double balance = client.getBalance(accountName);
-            return new BigDecimal(balance);
+            return new BigDecimal.valueOf(balance);
         } catch (BitcoinException e) {
             e.printStackTrace();
             return null;
